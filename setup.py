a8b1f42630fb7dabdc1b79cd402920b8a51f61f9
Nishant Raj <nishantraj8704@gmail.com>
2025-10-05 21:23:33 +0530

    requirement added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name ="Medical_Chatbot",
+    version="0.1.0",
+    author="Nishant Raj",
+    author_email = "nishantraj8704@gmail.com",
+    packages = find_packages(),
+    install_requires=[]
+)
